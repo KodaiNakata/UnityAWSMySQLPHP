7ec5a63d34dedfb91662be5bd642f17ce75ef909
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2022-07-28 23:06:03 +0900

    スコアの種類の列挙体を追加

---
+++ AWSMySQLPHP/Assets/Scripts/ScoreType.cs
@@ -0,0 +1,18 @@
+
+namespace UnityRanking
+{
+    /// <summary>
+    /// スコアの型
+    /// </summary>
+    public enum ScoreType
+    {
+        /// <summary>
+        /// 数値（整数、小数）
+        /// </summary>
+        Number,
+        /// <summary>
+        /// 時間
+        /// </summary>
+        Time
+    }
+}
