9599da4a5fce3580fe0d767523106e9200ee0383
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2022-08-02 23:33:01 +0900

    サーバーとの接続クラスを追加

---
+++ AWSMySQLPHP/Assets/Scripts/ServerConnecter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// サーバーとの接続クラス
+/// </summary>
+public class ServerConnecter
+{
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public ServerConnecter()
+    {
+
+    }
+}
