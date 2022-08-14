b4a2ec1bbff671a59fa9f8b334fc9d675004746c
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2022-08-14 22:57:32 +0900

    ランキングビューのプレハブを追加

---
+++ AWSMySQLPHP/Assets/Scripts/RankingViewNode.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+/// <summary>
+/// ランキングビューのノードクラス
+/// </summary>
+public class RankingViewNode : MonoBehaviour
+{
+    /// <summary>
+    /// 順位のテキスト
+    /// </summary>
+    public TextMeshProUGUI NoText;
+
+    /// <summary>
+    /// 名前のテキスト
+    /// </summary>
+    public TextMeshProUGUI NameText;
+
+    /// <summary>
+    /// スコアのテキスト
+    /// </summary>
+    public TextMeshProUGUI ScoreText;
+}
