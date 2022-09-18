557ba28ec1831702d34a0c5c95df4b43ee8f6828
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2022-09-18 19:06:37 +0900

    SQL文とbindValue修正と例外発生時のログに改行を追加

--- AWSMySQLPHP/Assets/Scripts/SelectScore.php
+++ AWSMySQLPHP/Assets/Scripts/SelectScore.php
@@ -10,28 +10,32 @@ $order_by = $_POST["orderBy"];
 
 try {
     // スコアの取得のSQLを実行
-    $query = 'SELECT * FROM score_tbl ';
+    $query = 'SELECT * FROM score_tbl ORDER BY score ';
     if ($order_by == 'OrderByAscending') {
-        $query .= 'ORDER BY ASC ';
+        $query .= 'ASC ';
     } else {
-        $query .= 'ORDER BY DESC ';
+        $query .= 'DESC ';
     }
     $query .= 'LIMIT :ranking_num;';
     $prepare = $pdo->prepare($query);
-    $prepare->bind_value(':ranking_num', $ranking_num, PDO::PARAM_INT);
-    $stmt = $pdo->execute();
+    $prepare->bindValue(':ranking_num', $ranking_num, PDO::PARAM_INT);
+    $prepare->execute();
 
-    // クライアント側に送るスコアのデータ
+    // スコアを取得
+    $stmt = $prepare->fetchAll(PDO::FETCH_ASSOC);
+
+    // スコアをクライアント側に送る用の変数に格納
     foreach ($stmt as $row) {
         $res = $res . $row['name'];
         $res = $res . $row['score'];
     }
 } catch (PDOException $e) {
-    echo 'スコアの取得失敗';
+    echo 'スコアの取得失敗'."\n";
     var_dump($e->getMessage());
 }
 
 $pdo = null; // DB切断
 
-echo $res; // クライアントに結果を返す
+print_r($res);
+//echo $res; // クライアントに結果を返す
 ?>
