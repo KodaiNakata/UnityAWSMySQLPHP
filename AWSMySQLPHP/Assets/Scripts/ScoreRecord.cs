88190e230015616d673a1a429ccd1abf47bab2e1
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2022-09-20 00:52:10 +0900

    DBからの値の取得用のクラスを追加

---
+++ AWSMySQLPHP/Assets/Scripts/ScoreRecord.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// スコアのレコード
+/// </summary>
+[System.Serializable]
+public class ScoreRecord
+{
+    public int score_id;
+    public string name;
+    public int score;
+}
