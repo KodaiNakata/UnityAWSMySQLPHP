dbbbe825afaf9fff681fc4d170bdd186c70eb096
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2022-11-05 23:17:52 +0900

    次にやることのTODOコメント付与

--- AWSMySQLPHP/Assets/Samples/SampleSceneManager.cs
+++ AWSMySQLPHP/Assets/Samples/SampleSceneManager.cs
@@ -8,7 +8,13 @@ public class SampleSceneManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        UnityRanking.RankingLoader.Instance.SendScoreAndShowRanking(200, ScoreOrderType.OrderByAscending, 10);
+        var score = 200;
+        UnityRanking.RankingLoader.Instance.SendScoreAndShowRanking(score, ScoreOrderType.OrderByAscending, 10);
+
+        //TODO：時間型のスコアの動作を試す。
+        // var time = 123456;
+        // var timeScore = new System.TimeSpan(0,0,0,0,time);
+        // UnityRanking.RankingLoader.Instance.SendTimeAndShowRanking(timeScore, ScoreOrderType.OrderByAscending, 10);
     }
 
     // Update is called once per frame
