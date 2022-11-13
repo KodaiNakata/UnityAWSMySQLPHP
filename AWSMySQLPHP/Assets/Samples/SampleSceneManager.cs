f033d2e16c25993500b2071798755db9ac6f32ba
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2022-11-13 16:46:15 +0900

    時間型のスコアと実数型のスコアの動作確認

--- AWSMySQLPHP/Assets/Samples/SampleSceneManager.cs
+++ AWSMySQLPHP/Assets/Samples/SampleSceneManager.cs
@@ -8,12 +8,12 @@ public class SampleSceneManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // var score = 200;
-        // UnityRanking.RankingLoader.Instance.SendScoreAndShowRanking(score, ScoreOrderType.OrderByAscending, 10);
+        var score = 200;
+        UnityRanking.RankingLoader.Instance.SendScoreAndShowRanking(score, ScoreOrderType.OrderByAscending, 10);
 
-        var time = 123456;
-        var timeScore = new System.TimeSpan(0,0,0,0,time);
-        UnityRanking.RankingLoader.Instance.SendTimeAndShowRanking(timeScore, ScoreOrderType.OrderByAscending, 10, "hh:mm:ss");
+        // var time = 123456;
+        // var timeScore = new System.TimeSpan(0,0,0,0,time);
+        // UnityRanking.RankingLoader.Instance.SendTimeAndShowRanking(timeScore, ScoreOrderType.OrderByAscending, 10, "hh:mm:ss");
     }
 
     // Update is called once per frame
